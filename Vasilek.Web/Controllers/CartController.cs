659a25980e39c32e3a58827d834931a7b30b59a0
Andrey_Gorlov <avgorlov899@gmail.com>
2022-06-01 01:26:14 +0300

    add CartController

---
+++ Vasilek.Web/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vasilek.Web.Services.IServices;
+
+namespace Vasilek.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IShoppingCartService _cartService;
+        public CartController(IProductService productService, IShoppingCartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+        public IActionResult CartIndex()
+        {
+            return View();
+        }
+    }
+}
