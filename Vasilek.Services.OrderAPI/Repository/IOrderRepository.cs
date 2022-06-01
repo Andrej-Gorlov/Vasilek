41852e2b9ed8443a36dd596bdabd1f2cef3fc17d
Andrey_Gorlov <avgorlov899@gmail.com>
2022-06-02 01:32:37 +0300

    add Repository

---
+++ Vasilek.Services.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Vasilek.Services.OrderAPI.Models;
+
+namespace Vasilek.Services.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+        Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+    }
+}
