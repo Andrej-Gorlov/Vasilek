08eec6fb95754e7608fb86e0a36400f61aba30a0
Andrey_Gorlov <avgorlov899@gmail.com>
2022-06-02 15:15:32 +0300

    add CouponRepository

---
+++ Vasilek.Services.ShoppingCart/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using Vasilek.Services.ShoppingCart.Models.Dto;
+
+namespace Vasilek.Services.ShoppingCart.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCoupon(string couponName);
+    }
+}
